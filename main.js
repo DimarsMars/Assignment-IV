b81c24247e716cc1ac8da6ac0f03195866abfd68
DimarsMars <dimasrangga620@gmail.com>
2024-10-16 23:49:24 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+let contentRight = document.getElementsByClassName("content-right");
+let buttonEdit = document.getElementsByName("button-edit-remove")
+// let display = 0;
+
+
+function hideShow() {
+    contentRight.style.display = "none";
+}
